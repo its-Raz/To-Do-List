b04055af89e83f1f5197533faf7b4a3d2b4770b4
razbi <razhaim101@gmail.com>
2023-01-02 19:15:17 +0200

    done with enque peek clone

--- src/LinkedListQueue.java
+++ src/LinkedListQueue.java
@@ -1,10 +1,24 @@
+import java.lang.reflect.Method;
+
 public class LinkedListQueue<T extends Cloneable> implements Queue<T> {
     Node<T> first;
     Node<T> last;
     public void enqueue(T element)
     {
-        
+        Node<T> newNode = new Node<T>(element);
+        if(isEmpty())
+        {
+            this.first = newNode;
+            this.last = first;
+        }
+        else
+        {
+            this.last.setNextNode(newNode);
+            this.last=newNode;
+        }
     }
+    public void setFirst(Node<T> node){this.first=node;}
+    public void setLast(Node<T> node){this.last=node;}
 
 
     public T dequeue()
@@ -13,7 +27,8 @@ public class LinkedListQueue<T extends Cloneable> implements Queue<T> {
     }
     public T peek()
     {
-       
+        if(this.first!=null){return this.first.getData();}
+        else{throw new EmptyQueueException();}
     }
 
     public int size()
@@ -25,7 +40,40 @@ public class LinkedListQueue<T extends Cloneable> implements Queue<T> {
 
     @Override
     public LinkedListQueue<T> clone() {
-      
+        try{
+            int listSize = this.size();
+            int counter=1;
+            LinkedListQueue<T> newList = new LinkedListQueue<T>();
+            if(this.first!=null) {
+                T clonedData;
+                Node<T> previousClonedNode = null;
+                for (T nodeData : this) {
+                    Class<T> cls = (Class<T>) nodeData.getClass(); // Get the Class object for the parameter type T
+                    Method cloneMethod = cls.getMethod("clone"); // Get the Method object for the clone() method
+                    clonedData = (T) cloneMethod.invoke(nodeData);// Invoke the clone() method on an instance of the data field
+                    Node<T> newNode = new Node<T>(clonedData);
+                    if (counter == 1) {
+                        newList.setFirst(newNode);
+                        if (listSize == 1) {
+                            newList.setLast(newNode);
+                        } else {
+                            previousClonedNode = newNode;
+                        }
+                    } else {
+                        previousClonedNode.setNextNode(newNode); //Set cloned Node to the previous node
+                        previousClonedNode = newNode;
+                        if (counter == listSize) {
+                            newList.setLast(newNode);
+                        }
+                    }
+                    counter++;
+                }
+            }
+            return newList;
+        }
+        catch(Exception exp){
+            return null;
+        }
     }
 
 
